123f80cac7f63ee8886f580be525c6c7734671ab
Yam <yam@gograb.io>
2024-05-24 20:17:07 -0600

    fix user

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,4 +1,4 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASS } from '@config';
 import { ConnectOptions } from 'mongoose';
 
 export interface IDatabase {
